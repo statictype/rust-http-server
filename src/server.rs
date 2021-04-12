989db25a4ca0623618b45f654a603e37d9af6714
Andreea Eftene <hi@pixelle.ro>
2021-04-12 19:46:39 +0200

    implement a custom trait to handle requests

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,15 @@
-use crate::http::{Request, Response, StatusCode};
+use crate::http::{ParseError, Request, Response, StatusCode};
 use std::convert::TryFrom;
 use std::io::Read;
 use std::net::TcpListener;
+
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+        println!("Failed to parse request: {}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
+}
 pub struct Server {
     addr: String,
 }
@@ -10,7 +18,7 @@ impl Server {
     pub fn new(addr: String) -> Self {
         Server { addr }
     }
-    pub fn run(self) {
+    pub fn run(self, mut handler: impl Handler) {
         println!("Listening on {}", self.addr);
         let listener = TcpListener::bind(&self.addr).unwrap();
         loop {
@@ -21,21 +29,10 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-                            // creates a slice that contains the entire array [..]
                             let response = match Request::try_from(&buffer[..]) {
-                                Ok(request) => {
-                                    // Request must implement the debug trait to be logged to the console
-                                    // rustc provide basic impl for some traits, like debug - use the derive attr
-                                    dbg!(request);
-                                    Response::new(
-                                        StatusCode::Ok,
-                                        Some("<h1>it works!</h1>".to_string()),
-                                    )
-                                }
-                                Err(err) => {
-                                    println!("There was an error: {}", err);
-                                    Response::new(StatusCode::BadRequest, None)
-                                }
+                                Ok(request) => handler.handle_request(&request),
+
+                                Err(err) => handler.handle_bad_request(&err),
                             };
 
                             if let Err(e) = response.send(&mut stream) {
