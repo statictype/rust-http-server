5ff4afe12ebf99163163b34fcf782d11e411822e
Andreea Eftene <hi@pixelle.ro>
2021-04-12 20:38:36 +0200

    serving html

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 // without ! it applies to the expression that follows
 #![allow(dead_code)]
 use server::Server;
+use std::env;
 use website_handler::WebsiteHandler;
 
 mod http;
@@ -9,6 +10,10 @@ mod server;
 mod website_handler;
 
 fn main() {
+    // cargo sets env vars at compile time, like the path of cargo.toml
+    let defaul_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    // fallback on default path if no env var was passed
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(defaul_path);
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,18 +1,26 @@
 use super::http::{Method, Request, Response, StatusCode};
 use super::server::Handler;
-pub struct WebsiteHandler;
+use std::fs;
+pub struct WebsiteHandler {
+    public_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
+    }
+}
+
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(
-                    StatusCode::Ok,
-                    Some("<h1>Welcome to homepage</h1>".to_string()),
-                ),
-                "/hello" => Response::new(
-                    StatusCode::Ok,
-                    Some("<h1>Welcome to another page</h1>".to_string()),
-                ),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
                 _ => Response::new(StatusCode::NotFound, None),
             },
             _ => Response::new(StatusCode::NotFound, None),
