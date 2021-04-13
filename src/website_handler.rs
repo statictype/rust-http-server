849589b1da7af179b6c77396d89820c47c2ce866
Andreea Eftene <hi@pixelle.ro>
2021-04-13 17:22:01 +0200

    serve files from public

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -11,7 +11,20 @@ impl WebsiteHandler {
     }
     fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
-        fs::read_to_string(path).ok()
+
+        // path must be validated, don't serve anything the user requests
+        // convert the path to absolute and check if it starts with public
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Direct traversal attack attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
     }
 }
 
@@ -21,7 +34,11 @@ impl Handler for WebsiteHandler {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
-                _ => Response::new(StatusCode::NotFound, None),
+
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
             },
             _ => Response::new(StatusCode::NotFound, None),
         }
